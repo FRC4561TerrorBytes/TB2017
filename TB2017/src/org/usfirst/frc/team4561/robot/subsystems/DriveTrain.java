4b94c4acd359d6659b3639d9afe9103fec2c499f
Lucas Therrien <ltherrien19@rthighschool.org>
2017-01-16 15:09:02 -0500

    LT UNTESTED:
    added code for tankdrive and arcadedrive

--- TB2017/src/org/usfirst/frc/team4561/robot/subsystems/DriveTrain.java
+++ TB2017/src/org/usfirst/frc/team4561/robot/subsystems/DriveTrain.java
@@ -2,11 +2,12 @@ package org.usfirst.frc.team4561.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;											//imports libraries
 import org.usfirst.frc.team4561.robot.RobotMap;
+import org.usfirst.frc.team4561.robot.commands.TankDrive;
+
 import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive; 
 
-public class DriveTrain extends Subsystem 
-{
+public class DriveTrain extends Subsystem {
 	CANTalon FrontRight = new CANTalon(RobotMap.FRONT_RIGHT_MOTOR_PORT);				//sets front motor ports
 	CANTalon FrontLeft = new CANTalon(RobotMap.FRONT_LEFT_MOTOR_PORT);			
 	CANTalon RearRight = new CANTalon(RobotMap.REAR_RIGHT_MOTOR_PORT);					//sets rear motor ports
@@ -14,9 +15,15 @@ public class DriveTrain extends Subsystem
 	
 	RobotDrive robotDrive = new RobotDrive(FrontLeft, RearLeft, FrontRight, RearRight);	//puts motors into robot drive class
 	
-;    public void initDefaultCommand() 
-    {
+;    public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+	setDefaultCommand(new TankDrive());
     }
+public void driveTank(double left, double right) {
+	robotDrive.tankDrive(left, right);
+}
+public void arcadeDrive(double drive, double rot ) {
+robotDrive.arcadeDrive(drive, rot);
+}
 }
