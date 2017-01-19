c1d9962ba548ab88f6c4a5d80e68ab196a120fc0
alekj <alekj@LAPTOP-IO7GV8K9>
2017-01-19 18:28:47 -0500

    AL - Untested - Commented and Stylized Shooter subsystem

--- TB2017/src/org/usfirst/frc/team4561/robot/commands/Shoot.java
+++ TB2017/src/org/usfirst/frc/team4561/robot/commands/Shoot.java
@@ -4,10 +4,14 @@ package org.usfirst.frc.team4561.robot.commands;
 import org.usfirst.frc.team4561.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-
+/**
+ * 
+ * @author alekj
+ *
+ */
 public class Shoot extends Command 
 {
-	private final double speed = 1;
+	private final double speed = 1; //The speed the shooter will run at when the button is pressed
 	public Shoot()
 	{
 		requires(Robot.shooter);
@@ -15,12 +19,12 @@ public class Shoot extends Command
 	
 	protected void execute()
 	{
-		Robot.shooter.ShootAtSpeed(speed);
+		Robot.shooter.shootAtSpeed(speed); //Run the shooter motor at the given speed
 	}
 	
 	protected void stop()
 	{
-		Robot.shooter.ShootAtSpeed(0);
+		Robot.shooter.shootAtSpeed(0); //Stop the shooter motor
 	}
 	
 	protected boolean isFinished()
--- TB2017/src/org/usfirst/frc/team4561/robot/subsystems/Shooter.java
+++ TB2017/src/org/usfirst/frc/team4561/robot/subsystems/Shooter.java
@@ -12,15 +12,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Shooter extends Subsystem {
 	
-	public CANTalon ShootMotor = new CANTalon(RobotMap.SHOOTER_MOTOR_PORT);
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	public CANTalon ShootMotor = new CANTalon(RobotMap.SHOOTER_MOTOR_PORT); //The Shooter's sole motor
+    
 
     public void initDefaultCommand() {
         setDefaultCommand(new Shoot());
     }
     
-    public void ShootAtSpeed(double speed){
+    public void shootAtSpeed(double speed){
+    	//If a valid value, run the shooter motor at the given speed
     	if (speed < 1.0 && speed > -1.0)    	ShootMotor.set(speed);
     }
     
