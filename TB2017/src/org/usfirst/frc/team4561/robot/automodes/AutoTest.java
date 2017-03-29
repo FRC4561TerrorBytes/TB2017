6ede6bd3d8439bf7f5b777d7d01d8678ab86db5a
Jonah T Wahl <jonahtwahl@gmail.com>
2017-03-29 19:03:36 -0400

    UNTESTED - JW: AutoTest changed for testing

--- TB2017/src/org/usfirst/frc/team4561/robot/automodes/AutoTest.java
+++ TB2017/src/org/usfirst/frc/team4561/robot/automodes/AutoTest.java
@@ -5,7 +5,9 @@ import org.usfirst.frc.team4561.robot.commands.ToggleDriveTrainPIDOff;
 import org.usfirst.frc.team4561.robot.commands.ToggleDriveTrainPIDOn;
 import org.usfirst.frc.team4561.robot.commands.TorqueGear;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  *
@@ -16,6 +18,14 @@ public class AutoTest extends CommandGroup {
     	addSequential(new ToggleDriveTrainPIDOn());
         addSequential(new TorqueGear());
         addSequential(new DriveTankTimed(-0.4, 0.4, 1.18));
+        int i = 0;				//create counter
+        while(i <= 5)
+        {
+        	addSequential(new DriveTankTimed(.1 , 0, .2));	//move left side
+        	addSequential(new WaitCommand(.3));
+        	addSequential(new DriveTankTimed(0, .1 , .2));	//move right side
+        	i++;				//increment counter
+        }
         addSequential(new ToggleDriveTrainPIDOff());
     }
 }
